d919ef7a5dbd10ebe05bb5b9af03d2c8680e73a3
Roberto Otero Galán <71451848+roberottt@users.noreply.github.com>
2020-11-13 16:51:46 -0500

    Add files via upload

---
+++ hostDiscovery.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in $(seq 2 254); do
+	#It redirects the output of ping to the /dev/null (the black hole)
+	timeout 1 bash -c "ping -c 1 192.168.1.$i > /dev/null" && echo "Host 192.168.1.$i is ACTIVE" &
+done; wait
