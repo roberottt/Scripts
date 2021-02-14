eca7f17ac16d54d71ed04b69d91f29fdf29fccba
Roberto Otero Galán <71451848+roberottt@users.noreply.github.com>
2021-02-14 22:25:41 +0100

    Update hostDiscovery.sh

--- hostDiscovery.sh
+++ hostDiscovery.sh
@@ -2,5 +2,5 @@
 
 for i in $(seq 2 254); do
 	#It redirects the output of ping to the /dev/null (the black hole)
-	timeout 1 bash -c "ping -c 1 192.168.1.$i 2>/dev/null" && echo "Host 192.168.1.$i is ACTIVE" &
+	timeout 1 bash -c "ping -c 1 192.168.1.$i > /dev/null" && echo "Host 192.168.1.$i is ACTIVE" &
 done; wait
